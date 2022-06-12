a41ddba009cde6b2026c17e63be96a8d6366a2a3
thisLife-hyeon <90155180+thisLife-hyeon@users.noreply.github.com>
2022-06-12 19:01:56 +0900

    피드백 수정예정

--- main.js
+++ main.js
@@ -65,3 +65,38 @@ function checkBoxes() {
     }
   });
 }
+
+//Scroll Magic Start
+
+// define images
+var images = ['img/bg-1.png', 'img/bg-2.png', 'img/bg-3.png', 'img/bg-4.png', 'img/bg-5.png'];
+// TweenMax can tween any property of any object. We use this object to cycle through the array
+var obj = { curImg: 0 };
+
+// create tween
+var tween = TweenMax.to(obj, 0.5, {
+  curImg: images.length - 1, // animate propery curImg to number of images
+  roundProps: 'curImg', // only integers so it can be used as an array index
+  repeat: 1, // repeat 3 times
+  immediateRender: true, // load first image automatically
+  ease: Linear.easeNone, // show every image the same ammount of time
+  onUpdate: function () {
+    $('#myimg').attr('src', images[obj.curImg]); // set the image source
+  },
+});
+
+// init controller
+var controller = new ScrollMagic.Controller();
+
+// build scene
+var scene = new ScrollMagic.Scene({ triggerElement: '#trigger', duration: 800 })
+  .setTween(tween)
+  // .addIndicators() // add indicators (requires plugin)
+  .addTo(controller);
+
+// handle form change
+$('form.move input[name=duration]:radio').change(function () {
+  scene.duration($(this).val());
+});
+
+//Scroll Magic End
